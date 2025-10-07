d9a1e69391a004221cde54a07dd2478863bab1cd
jorgsaul <jvelazquezp2301@alumno.ipn.mx>
2025-10-07 09:32:31 -0600

    adaptaciones al ejercicio

--- 05Ejercicio/js/funciones1.js
+++ 05Ejercicio/js/funciones1.js
@@ -10,10 +10,14 @@ function validarn (e){
 
 function interes(){
   var valor = document.getElementById("cantidadi").value;
+  var meses = document.getElementById('meses').value;
+  if(!valor || !meses) return alert('Llene los campos solicitados') //validacion de campos vacios
   var parseo = parseFloat(valor);
-  if(parseo < 100 || parseo > 1000000000) return alert('Ingrese un valor entre 100 y 10 mil millones'); //validacion de cantidad
-  var interes = parseo*0.085; //limite a 2 decimales
+  if(parseo < 100 || parseo > 1000000000) return alert('Ingrese un valor entre 100 y 10 mil millones de pesos'); //validacion de cantidad
+  var interes = parseo*0.02; //limite a 2 decimales
   var total  = interes + parseo;
+  parseo = parseFloat(meses);
+  if(parseo <3 || parseo > 18) return alert('La inversion debe de ser de minimo 3 y maximo 18 meses')
   document.getElementById("saldoi").value = "$ " + total; //limite a 2 decimales
 }
 
