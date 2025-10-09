fb8612a308f65fc0de906e25980bde86722bd61f
jorgsaul <jvelazquezp2301@alumno.ipn.mx>
2025-10-09 01:11:32 -0600

    Logica de portafolio 06 algoritmia

--- 06Algoritmia/js/algrotimia.js
+++ 06Algoritmia/js/algrotimia.js
@@ -1,5 +1,7 @@
 function problema1(){
-  //tarea
+  const datos = document.getElementById('p1-input').value;
+  const resultado = datos.trim().split(" ").map((dato) => dato.split('').reverse().join('')).toString()
+  document.getElementById('p1-output').textContent = resultado
 }
 
 function problema2(){
@@ -7,5 +9,23 @@ function problema2(){
 }
 
 function problema3(){
-  //tarea
+  const datos = document.getElementById('p3-input').value;
+  let letras = [];
+  let resultado = 0;
+  let palabra = "";
+  
+  let logica = datos.trim().split(',')
+  .map(valor => valor.split(''))
+  .forEach(arregloLetra => {
+    letras = []
+    arregloLetra.forEach(letra=> {
+      if(!letras.includes(letra)) letras.push(letra)
+    })
+    if(letras.length > resultado){
+      resultado = letras.length;
+      palabra = arregloLetra.join('');
+    }
+  });
+  
+  document.getElementById('p3-output').textContent = palabra;
 }
