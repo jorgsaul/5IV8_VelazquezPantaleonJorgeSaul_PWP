fa9cb367b243c99ac9b17f2957b9e71a1f7e0b57
jorgsaul <jvelazquezp2301@alumno.ipn.mx>
2025-11-02 19:40:25 -0600

    Terminacion de la api

---
+++ 12Examen/js/api.js
@@ -0,0 +1,44 @@
+const proxy = "https://cors-anywhere.herokuapp.com/";
+const URL_API = proxy + 'https://eightballapi.com/api?locale=es'
+
+function borrarCampos(){
+  document.getElementById("pregunta").value = "";
+}
+
+function validarCampos(pregunta){
+  let error = "";
+  if(!pregunta) error = "Por favor ingresa una pregunta.";
+  if (pregunta.length < 10) {
+    error = "La pregunta debe tener al menos 10 caracteres.";
+  }
+
+  if (!/[a-zA-Z]/.test(pregunta)) {
+    error = "La pregunta debe contener al menos una letra."; 
+  }
+
+  if (/^\d+$/.test(pregunta)) {
+    error = "La pregunta no debe contener solo números.";
+  }
+  
+  if(error){
+    alert(error);
+    return false;
+  }
+
+  return true
+}
+
+async function preguntar(){
+  const pregunta = document.getElementById('pregunta').value.trim();
+  if(!validarCampos(pregunta)) return;
+
+  try {
+    const response = await fetch(`${URL_API}&question=${pregunta}`);
+    const data = await response.json();
+    console.log(data)
+    const respuesta = data.reading;
+    document.getElementById('respuesta').value = respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+}
